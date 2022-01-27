9584e1fa33c299813543592b08629d96e1441eb0
Darshnik253 <85985007+Darshnik253@users.noreply.github.com>
2022-01-27 19:48:54 +0530

    Update Low_Pass_Filter.m

--- Octave_Code/Low_Pass_Filter.m
+++ Octave_Code/Low_Pass_Filter.m
@@ -1,8 +1,8 @@
 clc
 clear all;
 
+#Reading Excelsheet Data
 ReadXcel = xlsread('csv_matter.xlsx', 'Sheet1','A2:L1001');
-
 A_XOUT_H = ReadXcel(:,1);
 A_XOUT_L = ReadXcel(:,2);
 A_YOUT_H = ReadXcel(:,3);
@@ -10,14 +10,18 @@ A_YOUT_L = ReadXcel(:,4);
 A_ZOUT_H = ReadXcel(:,5);
 A_ZOUT_L = ReadXcel(:,6);
 
+#convert 2 8bit data of accelerometer into 16bit data
 res1 = double(typecast([uint8(A_XOUT_L), uint8(A_XOUT_H)], 'int16'))
 res2 = double(typecast([uint8(A_YOUT_L), uint8(A_YOUT_H)], 'int16'))
 res3 = double(typecast([uint8(A_ZOUT_L), uint8(A_ZOUT_H)], 'int16'))
 
+#set cuttoff frequency and controlling factor alpha
 fc = 100;
 RC = 1/(2*pi*fc) ;
 alpha =  0.01 / (RC+0.01);
 pkg load signal
+
+#part for unmapping of data scale range +/-2g
 Part = 39.2/65535;
 
 for i=1:((length(res1)))
@@ -33,6 +37,7 @@ t=0:0.01:9.99;
 subplot(3,1,1);
 plot(t,acc1);
 
+#filtering unmaped data of accx using low pass filter equation and plotting graph
 fil1(1)=acc1(1)
 for i=1:((length(acc1))-1)  
   fil1(i+1)=(alpha*acc1(i+1))+(fil1(i)*(1-alpha))
@@ -78,7 +83,7 @@ subplot(6,1,3);
 plot(t,acc3);
 
 
-fil(1)=acc3(1)
+fil3(1)=acc3(1)
 for i=1:((length(acc3))-1)  
   fil3(i+1)=(alpha*acc3(i+1))+(fil3(i)*(1-alpha))
 endfor
@@ -86,7 +91,4 @@ endfor
 t=0:0.01:9.99;
 subplot(6,2,3);
 plot(t,fil3)
-
-
-
 
